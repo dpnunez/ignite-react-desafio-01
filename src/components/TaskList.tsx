a0f86019e5367990e438b1199d1e7f010e3e8e09
Daniel Nunez <daniel.nunez@nave.rs>
2021-07-12 21:45:27 -0300

    feat: add create/remove/toggle logic

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,32 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+		if (!newTaskTitle) return
+		setTasks([...tasks, {
+			id: Math.random() * (9999999 - 1) + 1,
+			title: newTaskTitle,
+			isComplete: false
+		}])
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasks = tasks.map(task => {
+			if (task.id === id) {
+				return {
+					...task,
+					isComplete: !task.isComplete
+				}
+			}
+			return task
+		})
+
+		setTasks(newTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+		const filteredTasks = tasks.filter(task => task.id !== id)
+
+		setTasks(filteredTasks)
   }
 
   return (
